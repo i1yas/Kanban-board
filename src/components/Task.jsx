d371a3020b864134b42ada5ab032e72e6d58371a
i1yas <ilyasers@gmail.com>
2017-06-11 14:25:44 +0300

    Fix labels rendering

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Panel, Label, Media } from "react-bootstrap";
-import './Task.css';
+import "./Task.css";
 
 function Task(props) {
 	return (
@@ -8,44 +8,44 @@ function Task(props) {
 			<Panel header={props.title}>
 				<div className="Task__content">
 					<Media>
-						{props.image && (
+						{props.image &&
 							<Media.Left>
 								<img
 									src={props.image}
 									alt={props.title}
 									height={100}
 								/>
-							</Media.Left>)
-						}
+							</Media.Left>}
 						<Media.Body>
 							{props.children}
 						</Media.Body>
 					</Media>
 				</div>
-				<div className="Task__labels">
-					{props.labels &&
-						props.labels.map((label, ind) => {
-							return (
-								<span key={ind} className="Task__label">
-									<Label bsStyle="info">
-										{label}
-									</Label>
-									{"\u00A0"}
-								</span>
-							);
-						})}
-					{props.importantLabels &&
-						props.importantLabels.map((label, ind) => {
-							return (
-								<span key={ind} className="Task__label">
-									<Label bsStyle="primary">
-										{label}
-									</Label>
-									{"\u00A0"}
-								</span>
-							);
-						})}
-				</div>
+				{(props.labels || props.importantLabels) &&
+					<div className="Task__labels">
+						{props.labels &&
+							props.labels.map((label, ind) => {
+								return (
+									<span key={ind} className="Task__label">
+										<Label bsStyle="info">
+											{label}
+										</Label>
+										{"\u00A0"}
+									</span>
+								);
+							})}
+						{props.importantLabels &&
+							props.importantLabels.map((label, ind) => {
+								return (
+									<span key={ind} className="Task__label">
+										<Label bsStyle="primary">
+											{label}
+										</Label>
+										{"\u00A0"}
+									</span>
+								);
+							})}
+					</div>}
 			</Panel>
 		</div>
 	);
