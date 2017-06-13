43aeeb739181bf66c419f80bab7e05b4df7a6a5f
i1yas <ilyasers@gmail.com>
2017-06-13 16:18:48 +0300

    Fix navbar buttons layout, add Reset data button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Grid, Row, Navbar, Button, Glyphicon } from "react-bootstrap";
+import { Grid, Row, Navbar, Button, ButtonToolbar, Glyphicon } from "react-bootstrap";
 import BoardsContainer from "./components/BoardsContainer";
 import AddWindow from './components/AddWindow';
 
@@ -18,16 +18,21 @@ class App extends Component {
 
 				<AddWindow />
 
-				<Button className="App__add-button pull-right" bsStyle="primary" bsSize="large" >
-					<Glyphicon glyph="plus" /> Add Task
-				</Button>
-
 				<Navbar
 					className="App__footer"
 					fixedBottom
 					fluid
 				>
 					<Navbar.Brand className="App__footer-title">Kanban board</Navbar.Brand>
+
+					<ButtonToolbar className="App__buttons-toolbar pull-right">
+						<Button bsSize="large" >
+							<Glyphicon glyph="refresh" /> Reset data
+						</Button>
+						<Button bsStyle="primary" bsSize="large" >
+							<Glyphicon glyph="plus" /> Add Task
+						</Button>
+					</ButtonToolbar>
 				</Navbar>
 			</div>
 		);
