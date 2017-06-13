8e8a43a66de8c49ba171b0cb064113497675ee86
i1yas <ilyasers@gmail.com>
2017-06-13 15:50:05 +0300

    Add image field

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,38 +1,54 @@
 import React from "react";
-import { Form, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {
+	Form, FormGroup, FormControl, ControlLabel, InputGroup, DropdownButton, MenuItem
+} from "react-bootstrap";
 
 function AddForm(props) {
 	const allFields = ['title', 'content', 'labels', 'importantLabels'];
-	
+
 	let fields
-	if(props.fields === 'all') fields = allFields;
+	if (props.fields === 'all') fields = allFields;
 	else fields = props.fields;
 
 	return (
 		<Form>
 			{fields.indexOf('title') !== -1 &&
 				<FormGroup controlId="addFormName">
-					<ControlLabel>Title</ControlLabel> 
+					<ControlLabel>Title</ControlLabel>
 					<FormControl type="text" />
 				</FormGroup>
 			}
 			{fields.indexOf('content') !== -1 &&
 				<FormGroup controlId="addFormContent">
-					<ControlLabel>Task content</ControlLabel> 
+					<ControlLabel>Task content</ControlLabel>
 					<FormControl componentClass="textarea" />
 				</FormGroup>
 			}
 			{(fields.indexOf('labels') !== -1 || fields.indexOf('importantLabels') !== -1) && <hr />}
 			{fields.indexOf('labels') !== -1 &&
 				<FormGroup controlId="addFormLabels">
-					<ControlLabel>Labels</ControlLabel> 
+					<ControlLabel>Labels</ControlLabel>
 					<FormControl type="text" placeholder="list of labels separated by a comma" />
 				</FormGroup>
 			}
 			{fields.indexOf('importantLabels') !== -1 &&
 				<FormGroup controlId="addFormImportantLabels">
-					<ControlLabel>Important labels</ControlLabel> 
+					<ControlLabel>Important labels</ControlLabel>
 					<FormControl type="text" placeholder="list of labels separated by a comma" />
+				</FormGroup>
+			}
+			{fields.indexOf('image') !== -1 &&
+				<FormGroup>
+					<InputGroup>
+						<DropdownButton
+							componentClass={InputGroup.Button}
+							id="input-dropdown-addon"
+							title="Type"
+						>
+							<MenuItem key="1">URL</MenuItem>
+						</DropdownButton>
+						<FormControl type="text" placeholder="http://site.com/image.jpg"/>
+					</InputGroup>
 				</FormGroup>
 			}
 		</Form>
