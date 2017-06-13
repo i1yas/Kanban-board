9094881face41f30053a524634bc75c98a0d5e00
i1yas <ilyasers@gmail.com>
2017-06-13 21:03:09 +0300

    Add values in addForm

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -15,13 +15,13 @@ function AddForm(props) {
 			{fields.indexOf('title') !== -1 &&
 				<FormGroup controlId="addFormName">
 					<ControlLabel>Title</ControlLabel>
-					<FormControl type="text" />
+					<FormControl type="text" value={props.data && props.data.title} />
 				</FormGroup>
 			}
 			{fields.indexOf('content') !== -1 &&
 				<FormGroup controlId="addFormContent">
 					<ControlLabel>Task content</ControlLabel>
-					<FormControl componentClass="textarea" />
+					<FormControl componentClass="textarea" value={props.data && props.data.content} />
 				</FormGroup>
 			}
 
@@ -29,13 +29,21 @@ function AddForm(props) {
 			{fields.indexOf('labels') !== -1 &&
 				<FormGroup controlId="addFormLabels">
 					<ControlLabel>Labels</ControlLabel>
-					<FormControl type="text" placeholder="list of labels separated by a comma" />
+					<FormControl
+						type="text"
+						placeholder="list of labels separated by a comma"
+						value={props.data && props.data.labels}
+					/>
 				</FormGroup>
 			}
 			{fields.indexOf('importantLabels') !== -1 &&
 				<FormGroup controlId="addFormImportantLabels">
 					<ControlLabel>Important labels</ControlLabel>
-					<FormControl type="text" placeholder="list of labels separated by a comma" />
+					<FormControl
+						type="text"
+						placeholder="list of labels separated by a comma"
+						value={props.data && props.data.importantLabels}
+					/>
 				</FormGroup>
 			}
 
@@ -51,7 +59,11 @@ function AddForm(props) {
 						>
 							<MenuItem key="1">URL</MenuItem>
 						</DropdownButton>
-						<FormControl type="text" placeholder="http://site.com/image.jpg"/>
+						<FormControl
+							type="text"
+							placeholder="http://site.com/image.jpg"
+							value={props.data && props.data.image}
+						/>
 					</InputGroup>
 				</FormGroup>
 			}
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -28,6 +28,10 @@ class Task extends Component {
 	render() {
 		const allFields = ['title', 'content', 'labels', 'importantLabels', 'image'];
 		const usedFields = allFields.filter(field => this.props[field]);
+		const data = {};
+		for(let i = 0; i < usedFields.length; i++) {
+			data[usedFields[i]] = this.props[usedFields[i]];
+		}
 
 		return (
 			<div className="Task" onClick={this.handleClick.bind(this)}>
@@ -79,7 +83,10 @@ class Task extends Component {
 
 					{this.state.isEdit &&
 						<div className="Task__editForm">
-							<AddForm fields={usedFields} />
+							<AddForm
+								fields={usedFields}
+								data={data}
+							/>
 							<ButtonToolbar className="Task__editForm-buttons pull-right">
 								<Button>Cancel</Button>
 								<Button bsStyle="primary">Save</Button>
@@ -91,64 +98,5 @@ class Task extends Component {
 		);
 	}
 }
-/*function Task(props) {
-	const allFields = ['title', 'content', 'labels', 'importantLabels', 'image'];
-	const usedFields = allFields.filter(field => props[field]);
-
-	return (
-		<div className="Task">
-			<Panel header={props.title}>
-				<div className="Task__content">
-					<Media>
-						{props.image &&
-							<Media.Left>
-								<img
-									src={props.image}
-									alt={props.title}
-									height={100}
-								/>
-							</Media.Left>}
-						<Media.Body>
-							{props.content}
-						</Media.Body>
-					</Media>
-				</div>
-				{(props.labels || props.importantLabels) &&
-					<div className="Task__labels">
-						{props.labels &&
-							props.labels.map((label, ind) => {
-								return (
-									<span key={ind} className="Task__label">
-										<Label bsStyle="info">
-											{label}
-										</Label>
-										{"\u00A0"}
-									</span>
-								);
-							})}
-						{props.importantLabels &&
-							props.importantLabels.map((label, ind) => {
-								return (
-									<span key={ind} className="Task__label">
-										<Label bsStyle="primary">
-											{label}
-										</Label>
-										{"\u00A0"}
-									</span>
-								);
-							})}
-					</div>}
-					
-				<div className="Task__editForm">
-					<AddForm fields={usedFields}/>
-					<ButtonToolbar className="Task__editForm-buttons pull-right">
-						<Button>Cancel</Button>
-						<Button bsStyle="primary">Save</Button>
-					</ButtonToolbar>
-				</div>			
-			</Panel>
-		</div>
-	);
-}*/
 
 export default Task;
